7e9ef593b43ee4d0dcdeb243ac864b032867ac0e
Ayush Shashikant Jadhav <ayushsjadhav99@gmail.com>
2024-12-16 07:36:26 +0530

    final commit

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ Socketio = SocketIO(app)
 
 rooms = {}
 
-def generate_unique_code(Length):
+def generate_unique_code(length):
     while True:
-        for _ in range(Length):
-            code=random.choice(ascii_uppercase)
+        code = ""
+        for _ in range(length):
+            code += random.choice(ascii_uppercase)
             
         if code not in rooms:
             break
@@ -48,10 +49,60 @@ def home():
         
     return render_template("home.html")
 
+
 @app.route("/room")
 def room():
-    return render_template("room.html")
+    room = session.get("room")
+    if room is None or session.get("name") is None or room not in rooms:
+        return redirect(url_for("home"))
+    
+    return render_template("room.html", code=room, messages=rooms[room]["messages"])
+
+@Socketio.on("message")
+def message(data):
+    room = session.get("room")
+    if room not in rooms:
+        return
+    
+    content = {
+        "name": session.get("name"),
+        "message": data["data"]
+    }
+    send(content, to=room)
+    rooms[room]["messages"].append(content)
+    print(f"{session.get("name")} said : {data["data"]}")
 
 
+
+@Socketio.on("connect")
+def connect(auth):
+    room = session.get("room")
+    name = session.get("name")
+    
+    if not room or not name:
+        return
+    if room not in rooms:
+        leave_room(room)
+        return
+    
+    join_room(room)
+    send({"name":name, "message":"has entered the room"}, to=room)
+    rooms[room]["members"] += 1
+    print(f"{name} joined room {room}")
+    
+@Socketio.on("disconnect")
+def disconnect():
+    room = session.get("room")
+    name = session.get("name")
+    
+    if room in rooms:
+        rooms[room]["members"] -= 1
+        if rooms[room]["members"]<=0:
+            del rooms[room]
+    
+    send({"name":name, "message":"has left the room"}, to=room)
+    print(f"{name} has left the room {room}")
+
+    
 if __name__ == "__main__":
     Socketio.run(app, debug=True)
